83e1d4a1b797ad0222fb8eccb5a6830dcf9443ec
EdmundoRdm <edmundoramos28@hotmail.com>
2024-10-07 17:54:49 -0600

    Creacion de landing
    Se crea la pagina del landing, básica y sin funciones. Está será la base para el repositorio

---
+++ Komalli/Komalli/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Komalli
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        // Cambia el cursor a "Mano" cuando el mouse pasa por la imagen
+        private void Image_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Cursor = Cursors.Hand;
+        }
+
+        // Vuelve el cursor a "Flechita" cuando el mouse deja la imagen
+        private void Image_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Cursor = Cursors.Arrow;
+        }
+    }
+}
