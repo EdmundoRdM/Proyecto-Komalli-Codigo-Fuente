ead50ad34cfddc60e5acc45a19ebb1c01f2404a2
Fernando-G3 <nando2015.jfgo@gmail.com>
2024-10-08 18:04:22 -0600

    LandingManager
    Se modifico el landing del gerente

---
+++ Komalli/Komalli/GUIs/ManagerLanding.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Komalli.GUIs
+{
+    /// <summary>
+    /// Lógica de interacción para ManagerLanding.xaml
+    /// </summary>
+    public partial class ManagerLanding : Window
+    {
+        public ManagerLanding()
+        {
+            InitializeComponent();
+        }
+        private void Image_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Cursor = Cursors.Hand;
+        }
+
+        // Vuelve el cursor a "Flechita" cuando el mouse deja la imagen
+        private void Image_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Cursor = Cursors.Arrow;
+        }
+    }
+}
