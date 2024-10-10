4ee2b470749e96078c578ca87596cccfdc05a67b
EdmundoRdm <edmundoramos28@hotmail.com>
2024-10-09 23:16:50 -0600

    Estandarización del MainWindow y Creación de conexión de BD
    Se creo el mapeo a base de datos mediante entity framework. Se usarán pages que se incrustarán en un viewbox para que se reacomode en la ventana mazimizada. Se incluye la pagina del inicio de sesión con funciones básicas (Aún no se verifica ni se accede al sistema, solo tiene validaciones de campos vacíos.

---
+++ Komalli/Komalli/DataBaseManagement/DataAccessObject/StaffDAO.cs
@@ -0,0 +1,140 @@
+﻿using Komalli.DataBaseManagement.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Komalli.DataBaseManagement.DataAccessObject
+{
+    internal class StaffDAO
+    {
+        public int RegisterStaff(Staff staff)
+        {
+            int result = 0;
+            using (var context = new KomalliDBEntities())
+            {
+                using (var transaction = context.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        context.Staffs.Add(staff);
+                        result = context.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (EntityException ex)
+                    {
+                        transaction.Rollback();
+                        throw new EntityException("Error al acceder a la base de datos.", ex);
+                    }
+                    catch (DbUpdateException ex)
+                    {
+                        transaction.Rollback();
+                        throw new DbUpdateException("Error al actualizar la base de datos.", ex);
+                    }
+                }
+            }
+            return result;
+        }
+        public int ModifyStaff(Staff staff)
+        {
+            int result = 0;
+            using (var context = new KomalliDBEntities())
+            {
+                using (var transaction = context.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        var originalStaff = context.Staffs.FirstOrDefault(s => s.EmployeeNumber == staff.EmployeeNumber);
+                        if (originalStaff != null)
+                        {
+                            originalStaff.FirstName = staff.FirstName;
+                            originalStaff.LastName = staff.LastName;
+                            originalStaff.MiddleName = staff.MiddleName;
+                            originalStaff.Password = staff.Password;
+                            originalStaff.Role = staff.Role;
+
+                            result = context.SaveChanges();
+                            transaction.Commit();
+                        }
+                    }
+                    catch (EntityException ex)
+                    {
+                        transaction.Rollback();
+                        throw new EntityException("Error al acceder a la base de datos.", ex);
+                    }
+                    catch (DbUpdateException ex)
+                    {
+                        transaction.Rollback();
+                        throw new DbUpdateException("Error al actualizar la base de datos.", ex);
+                    }
+                }
+            }
+            return result;
+        }
+
+        public int DeleteStaff(int employeeNumber)
+        {
+            int result = 0;
+            using (var context = new KomalliDBEntities())
+            {
+                using (var transaction = context.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        var staff = context.Staffs.FirstOrDefault(s => s.EmployeeNumber == employeeNumber);
+                        if (staff != null)
+                        {
+                            context.Staffs.Remove(staff);
+                            result = context.SaveChanges();
+                            transaction.Commit();
+                        }
+                    }
+                    catch (EntityException ex)
+                    {
+                        transaction.Rollback();
+                        throw new EntityException("Error al acceder a la base de datos.", ex);
+                    }
+                    catch (DbUpdateException ex)
+                    {
+                        transaction.Rollback();
+                        throw new DbUpdateException("Error al actualizar la base de datos.", ex);
+                    }
+                }
+            }
+            return result;
+        }
+
+        public int LogIn(string username, string password)
+        {
+            int result = 0;
+            try
+            {
+                using (var context = new KomalliDBEntities())
+                {
+                    var staff = context.Staffs.FirstOrDefault(s => s.FirstName == username && s.Password == password);
+                    if (staff != null)
+                    {
+                        result = 0; 
+                    }
+                    else
+                    {
+                        result = 1;                     }
+                }
+            }
+            catch (EntityException ex)
+            {
+                result = 5;
+                throw new EntityException("Error al acceder a la base de datos.", ex);
+            }
+            catch (InvalidOperationException ex)
+            {
+                result = 5;
+                throw new InvalidOperationException("Error al procesar la solicitud.", ex);
+            }
+            return result;
+        }
+    }
+}
---
+++ Komalli/Komalli/GUIs/LogIn.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Komalli.GUIs
+{
+    /// <summary>
+    /// Lógica de interacción para LogIn.xaml
+    /// </summary>
+    public partial class LogIn : Page
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void LogIng_Click(object sender, RoutedEventArgs e)
+        {
+            int fieldsOk = ValidateFields();
+            string username = "";
+            string password = "";
+            switch (fieldsOk)
+            {
+                case 0:
+                    break;
+
+                case 1:
+                    // Contraseña oculta, ambos campos están llenos (Usuario y PasswordBox)
+                    // Aquí puedes ejecutar la lógica de inicio de sesión normal
+                    MessageBox.Show("Inicio de sesión con contraseña oculta exitoso.", "Exito", MessageBoxButton.OK, MessageBoxImage.Information);
+                    username = txtUserName.Text;
+                    password = pwdPassword.Password;
+                    break;
+                case 2:
+                    // Contraseña visible, ambos campos están llenos (Usuario y TextBox de la contraseña visible)
+                    // Lógica de inicio de sesión para contraseña visible
+                    MessageBox.Show("Inicio de sesión con contraseña visible exitoso.", "Exito", MessageBoxButton.OK, MessageBoxImage.Information);
+                    username = txtUserName.Text;
+                    password = txtPassword.Text;
+                    break;
+
+                default:
+                    MessageBox.Show("Ocurrió un error inesperado.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    break;
+            }
+        }
+
+        private int ValidateFields()
+        {
+            int result = 0;
+            if(txtPassword.Visibility == Visibility.Visible)
+            {
+                if (string.IsNullOrWhiteSpace(txtUserName.Text) || string.IsNullOrWhiteSpace(txtPassword.Text))
+                {
+                    MessageBox.Show("Por favor escriba su usuario y contraseña para continuar", "Campos requeridos", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+                else
+                {
+                    result = 2;
+                }
+            }
+            else
+            {
+                if (string.IsNullOrWhiteSpace(txtUserName.Text) || string.IsNullOrWhiteSpace(pwdPassword.Password))
+                {
+                    MessageBox.Show("Por favor escriba su usuario y contraseña para continuar", "Campos requeridos", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+                else
+                {
+                    result = 1;
+                }
+            }
+            return result;
+        }
+
+        private void HidePassword(object sender, MouseButtonEventArgs e)
+        {
+            txtPassword.Text = pwdPassword.Password;
+
+            pwdPassword.Visibility = Visibility.Collapsed;
+            txtPassword.Visibility = Visibility.Visible;
+
+            btnHidePassword.Visibility = Visibility.Collapsed;
+            btnSeePassword.Visibility = Visibility.Visible;
+
+
+        }
+        private void SeePassword(object sender, MouseButtonEventArgs e)
+        {
+            pwdPassword.Password = txtPassword.Text;
+
+            pwdPassword.Visibility = Visibility.Visible;
+            txtPassword.Visibility = Visibility.Collapsed;
+
+            btnHidePassword.Visibility = Visibility.Visible;
+            btnSeePassword.Visibility = Visibility.Collapsed;
+
+        }
+
+    }
+}
