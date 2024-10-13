e6363644a2f2264d4269215af66c96185aed2884
Fernando-G3 <nando2015.jfgo@gmail.com>
2024-10-13 00:54:24 -0600

    Casos de uso completo
    Incluye la implementacion de todos los casos de uso correspondientes a mi parte

---
+++ Komalli/Komalli/DataBaseManagement/DataAccessObject/RoleDAO.cs
@@ -0,0 +1,36 @@
+﻿using Komalli.DataBaseManagement.DataModel;
+using Komalli.DataBaseManagement.POCOs;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Komalli.DataBaseManagement.DataAccessObject
+{
+    internal class RoleDAO
+    {
+        public List<RolePOCO> GetAllRoles()
+        {   
+            try
+            {
+                using (var context = new KomalliDBEntities())
+                {
+                    var roleList = context.Roles.ToList();
+                    var rolePOCOList = roleList.Select(role => new RolePOCO
+                    {
+                        RoleId = role.RoleId,
+                        RoleName = role.Role1
+                    }).ToList();
+
+                    return rolePOCOList;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al registrar el empleado.", ex);
+            }
+        }
+    }
+}
