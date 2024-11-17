16556b05b3a4b1ff4ac1013864602dc870728337
Fernando-G3 <nando2015.jfgo@gmail.com>
2024-11-17 16:23:33 -0600

    Arreglos
    Se arreglaron algunos problemas y se agrego la vista de Pedidos del cajero

---
+++ Komalli/Komalli/GUIs/OrdersModuleCook.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Komalli.GUIs
+{
+    /// <summary>
+    /// Lógica de interacción para OrdersModuleCook.xaml
+    /// </summary>
+    public partial class OrdersModuleCook : Page
+    {
+        public OrdersModuleCook()
+        {
+            InitializeComponent();
+        }
+    }
+}
