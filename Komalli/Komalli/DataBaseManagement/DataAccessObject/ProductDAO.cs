b3f9002fe4d431591bde1ffcc4abbaaa0e448776
FerdyReVive <ferdyrevive@hotmail.com>
2024-11-17 05:32:50 -0600

    POCO y DAO de Producto
    Se creo el POCO y la base del DAO de producto

---
+++ Komalli/Komalli/DataBaseManagement/DataAccessObject/ProductDAO.cs
@@ -0,0 +1,45 @@
+﻿using Komalli.DataBaseManagement.DataModel;
+using Komalli.DataBaseManagement.POCOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Komalli.DataBaseManagement.DataAccessObject
+{
+    internal class ProductDAO
+    {
+        public List<ProductPOCO> GetProductByName(string partialName)
+        {
+            try
+            {
+                using (var context = new KomalliDBEntities())
+                {
+                    var productList = (from product in context.Product
+                                    where product.Status == true &&
+                                           product.Name.ToLower().Contains(partialName.ToLower())
+                                     orderby product.Name descending
+                                     select new ProductPOCO
+                                     {
+                                         ProductId = product.ProductId,
+                                         ProductAvailableQuantity = product.AvailableQuantity,
+                                         ProductPrice = product.Price,
+                                         ProductDescription = product.Description,
+                                         ProductName = product.Name,
+                                         ProductFromKitchen = product.FromKitchen,
+                                         ProductSellingDate = (DateTime)product.SellingDate,
+                                         ProductStatus = product.Status,
+                                         ProductTypeId = product.Type
+                                     })
+                             .ToList();
+                    return productList;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al obtener el personal por nombre parcial.", ex);
+            }
+        }
+    }
+}
---
+++ Komalli/Komalli/DataBaseManagement/POCOs/ProductPOCO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Komalli.DataBaseManagement.POCOs
+{
+    internal class ProductPOCO
+    {
+        public int ProductId { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int ProductAvailableQuantity { get; set; }
+        public String ProductDescription { get; set; }
+        public String ProductName { get; set; }
+        public int ProductTypeId { get; set; }
+        public String ProductType { get; set; }
+        public Boolean ProductStatus { get; set; }
+        public DateTime ProductSellingDate { get; set; }
+        public Boolean ProductFromKitchen { get; set; }
+
+    }
+}
